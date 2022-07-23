4dee8d25fe818152a7d225f34ddc6b287023e2ef
sam <fangjzh@foxmail.com>
2022-07-23 10:15:27 +0800

    print result as markdown

---
+++ Lammps_bench/print_result_markdown.sh
@@ -0,0 +1,1 @@
+cat performance.txt | grep Performance | awk 'BEGIN{print "| # of cores | ns/day | hours/ns | timeseteps/s |";print "| ---- | ---- | ---- |"}{print "| " NR " | " $2 " | " $4 " | "$6 " |"}'
