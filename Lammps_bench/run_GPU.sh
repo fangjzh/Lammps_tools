6d1ca7081760360675b6af94bc38a121dd1e3995
sam <fangjzh@foxmail.com>
2022-07-27 19:57:58 +0800

    添加了GPU测试

--- Lammps_bench/run_GPU.sh
+++ Lammps_bench/run_GPU.sh
@@ -8,8 +8,8 @@ num_cores=$(lscpu | awk '{if($1=="CPU(s):"){total=$2}}{if($1=="Thread(s)"){p=$4}
 for num in $(seq 1 $num_cores)
 do
 echo $num >> performance.txt
-mpirun -np $num $program -i in.eam 
+#mpirun -np $num $program -i in.eam 
 ## gpu example 
-#  mpirun -np $num $program -sf gpu -pk gpu 1 -i in.eam
-cat log.lammps | grep  Performance >> performance.txt
+mpirun -np $num $program -sf gpu -pk gpu 1 -i in.eam
+cat log.lammps | grep  Performance | grep -v Implementing >> performance.txt
 done
